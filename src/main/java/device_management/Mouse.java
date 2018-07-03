e67003afb6244e5abf1c36fd78975a8e4a759811
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-03 11:48:05 +0100

    mouse setup

--- src/main/java/device_management/Mouse.java
+++ src/main/java/device_management/Mouse.java
@@ -1,4 +1,22 @@
 package device_management;
 
-public class Mouse {
+import behaviours.IInput;
+
+public class Mouse implements IInput {
+
+    private String type;
+    private int numberOfButtons;
+
+    public Mouse(String type, int numberOfButtons){
+        this.type = type;
+        this.numberOfButtons = numberOfButtons;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getNumberOfButtons() {
+        return numberOfButtons;
+    }
 }
--- src/test/java/MouseTest.java
+++ src/test/java/MouseTest.java
@@ -1,2 +1,25 @@
+import device_management.Mouse;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MouseTest {
+
+    Mouse mouse;
+
+    @Before
+    public void before(){
+        mouse = new Mouse("wireless", 2);
+    }
+
+    @Test
+    public void canGetType(){
+        assertEquals("wireless", mouse.getType());
+    }
+
+    @Test
+    public void canGetNumberOfButtons(){
+        assertEquals(2, mouse.getNumberOfButtons());
+    }
 }
